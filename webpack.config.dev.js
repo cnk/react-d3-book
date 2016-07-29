bc75dde0918c5a351899639b2203e0f839b79e27
Cynthia Kiser <cnk@caltech.edu>
2016-07-29 12:20:30 -0700

    Added support for compiling less -> css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,11 +21,18 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.js$|\.jsx$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.less$/,
+      loader: "style!css!less"
     }]
   }
 };
